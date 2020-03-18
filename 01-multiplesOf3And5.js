9585b776c66e82c441cd1957b772072acabb8161
Tim <timculpcreations@gmail.com>
2020-03-18 15:11:24 -0400

    03-18-2020 Problem Solving

--- 01-multiplesOf3And5.js
+++ 01-multiplesOf3And5.js
@@ -1,6 +1,11 @@
+/*
+The variable 'i' is the current number we're considering, and the variable 'sum' is the sum of all numbers that follow the rule. We exhaustively count out the set of numbers from 0 to 1000, and if a number is evenly divisible by either 3 or 5, we add it to the sum.
+*/
+
 let sum, i;
 
 sum = 0;
+
 console.log("The set includes:")
 for (i = 0; i < 1000; i ++) {
     if (i % 3 === 0 || i % 5 === 0) {
