9a483dc18ab74b53a2e59a5d35a74faa2a961ac1
Tim <timculpcreations@gmail.com>
2020-03-22 12:16:47 -0400

    03-22-2020 smallest multiple of first 20 nat numbers

--- 05-smallestMultiple.js
+++ 05-smallestMultiple.js
@@ -1,28 +1,35 @@
-//2520
-
 /* WHITEBOARD FOR BRAINSTORMING
-        20 - 5, 2, 2
-19 - 19
-    18 - 3, 3, 2
-17 - 17
-16 - 2, 2, 2, 2
-        15 - 5, 3
-13 - 13
-        12 - 3, 2, 2
-11 - 11
-        10 - 5, 2
-9 - 3, 3
-        8 - 2, 2, 2
-7 - 7
-                6 - 3, 2
-5 - 5
-                4 - 2, 2
-                3 - 3
-                2 - 2
-if we confirm the some factors, we can rule out the need to confirm other factors
+20 - 5, 2, 2
+*19 - 19
+18 - 3, 3, 2
+*17 - 17
+*16 - 2, 2, 2, 2
+15 - 5, 3
+*13 - 13
+12 - 3, 2, 2
+*11 - 11
+10 - 5, 2
+*9 - 3, 3
+8 - 2, 2, 2
+*7 - 7
+6 - 3, 2
+*5 - 5
+4 - 2, 2
+3 - 3
+2 - 2
+
+We must confirm whether a number we consider is a multiple of our 20 integers.
+If we confirm some factors, we can rule out the need to confirm other factors.
+Prime factors are required unless a multiple of them is confirmed.
+Because 16 is a multiple of 8, 4, and 2, we don't have to confirm whether our number is divisible by 8, 4, or 2.
+Since 2 is confirmed by the confirmation of 16, all other even numbers are confirmed if their halves are confirmed.
+3 is confirmed if 9 is confirmed.
+18 is confirmed if 9 and 16 are confirmed.
+20 is confirmed if 5 and 16 are confirmed.
+15 is confirmed if 9 and 5 are confirmed.
 */
 
-//we start with the multiple of what are determined to be essential factors and go down from there to make sure there are no others
+//we start with the multiple of the factors that are determined to be essential and go down from there to make sure there are no other smaller multiples
 let smallestMultiple;
 let startingMultiple = 19n*17n*16n*13n*11n*9n*7n*5n;
 console.log("start: ", startingMultiple);
